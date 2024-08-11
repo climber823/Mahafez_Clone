8b423cdfcfc7448dbe9b75c4e0c16804de12ecd0
orest <orestzelisko49@gmail.com>
2024-08-11 02:02:46 -0500

    update package.json

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { SELECT_ASSET, LOGIN, LOGOUT } from './actions';
 
 const initialState = {
-  selectedAsset: 'Copper',
+  selectedAsset: 'AUD/USD',
   isLoggedIn: true,
 };
 
