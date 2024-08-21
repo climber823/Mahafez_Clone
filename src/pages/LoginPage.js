eba4c9f34bea6d8ff21760cfdf30eb79eea3ac55
orest <orestzelisko49@gmail.com>
2024-08-21 12:03:29 -0500

    push updates

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -18,7 +18,7 @@ const LoginPage = () => {
  
   const handleLogin = async (id, pwd) => {
     // axios.post(`${process.env.REACT_APP_API_URL_DEPLOY}/api/auth/login`, {
-    axios.post(`http://localhost:5000/api/auth/login`, {
+    axios.post(`/server/api/auth/login`, {
       account_id: id, 
       password: pwd
     })
